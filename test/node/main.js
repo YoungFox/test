ee64df41b8ed700972133ba389a5c13488cda8bc
wenliang.yang <wenliang.yang@bitmain.com>
2019-03-26 17:41:36 +0800

    happy copy

--- test/node/main.js
+++ test/node/main.js
@@ -1,5 +1,8 @@
 var counter1 = require('./util/counter');
 var counter2 = require('./util/counter');
+var http = require('http')
+
+const url = require('url')
 
 console.log(counter1.count());
 console.log(counter2.count());
@@ -7,3 +10,28 @@ console.log(counter1.count());
 console.log(counter2.count());
 
 console.log(process.argv)
+
+console.log(url.parse('http://user:pass@host.com.8080/p/a/t/h?query=string#hash'))
+
+// http.createServer(function (request, response){
+//     var body = []
+//     console.log(request.method)
+//     console.log(request.headers)
+
+//     request.on('data', function (chunk){
+//         body.push(chunk)
+//     })
+
+//     request.on('end', function(){
+//         body = Buffer.concat(body)
+//         console.log(body.toString())
+//     }).listen(80)
+// })
+
+process.on('uncaughtException', function (err) {
+    console.log('Error: %s', err.message);
+});
+
+setTimeout(function (fn) {
+    fn();
+});
---
+++ test/node/server.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+const path = require('path')
+const http = require('http')
+
+let MIME = {
+    '.css': 'text/css',
+    '.js': 'application/javascript'
+}
+
+function main(argv) {
+    var config = JSON.parse(fs.readFileSync(argv[0], 'utf-8'))
+    var root = config.root || '.';
+    var port = config.port || 80;
+
+    http.createServer(function (request, response) {
+
+        var urlInfo = parseURL(root, request.url)
+        // console.log(urlInfo)
+
+        combineFiles(urlInfo.pathnames, function (err, data) {
+            if (err) {
+                response.writeHead(404)
+                response.end(err.message)
+            } else {
+                response.writeHead(200, {
+                    'Content-Type': urlInfo.mime
+                })
+                response.end(data)
+            }
+        })
+
+    }).listen(port)
+}
+
+function parseURL(root, url) {
+    let base, pathnames, parts;
+
+    if (url.indexOf('??') === -1) {
+        url = url.replace('/', '/??');
+    }
+
+    parts = url.split('??')
+    base = parts[0]
+
+    pathnames = parts[1].split(',').map(function (value) {
+        return path.join(root, base, value)
+    })
+
+    return {
+        mime: MIME[path.extname(pathnames[0])] || 'text/plain',
+        pathnames
+    }
+}
+
+function combineFiles(pathnames, callback) {
+    let output = [];
+    (function next(i, len) {
+        if (i < len) {
+            fs.readFile(pathnames[i], function (err, data) {
+                if (err) {
+                    callback(err)
+                } else {
+                    output.push(data)
+                    next(i + 1, len)
+                }
+            })
+        } else {
+            callback(null, Buffer.concat(output))
+        }
+    })(0, pathnames.length)
+}
+
+main(process.argv.slice(2))

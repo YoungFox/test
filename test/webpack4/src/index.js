7b219412e1fe80c48dd457adc630abc89a53e8b8
wenliang.yang <wenliang.yang@bitmain.com>
2019-12-04 18:18:05 +0800

    新增资源

--- test/webpack4/src/index.js
+++ test/webpack4/src/index.js
@@ -1,11 +1,24 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './icon.png'
+import Data from './data.xml'
 function component() {
     var element = document.createElement('div');
 
     // 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.classList.add('hello');
 
-    return element;
+    // 将图像添加到我们现有的div.
+    var myIcon = new Image()
+
+    myIcon.src = Icon
+
+    element.appendChild(myIcon)
+
+    console.log(Data)
+    
+    return element
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())

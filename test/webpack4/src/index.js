e718c6b3f863686950090d94a2f88b7d876e5228
wenliang.yang <wenliang.yang@bitmain.com>
2019-12-04 17:39:25 +0800

    无配置的情况

--- test/webpack4/src/index.js
+++ test/webpack4/src/index.js
@@ -1,7 +1,8 @@
+import _ from 'lodash'
 function component() {
     var element = document.createElement('div');
 
-
+    // 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
     return element;

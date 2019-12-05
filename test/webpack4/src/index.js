cdac49e69a6a3c84efe2f1ae4204193dcb061996
wenliang.yang <wenliang.yang@bitmain.com>
2019-12-05 09:54:37 +0800

    管理输出

--- test/webpack4/src/index.js
+++ test/webpack4/src/index.js
@@ -1,23 +1,17 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './icon.png'
-import Data from './data.xml'
+import printMe from './print.js'
+
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('buttotn')
 
-    // 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    element.classList.add('hello');
 
-    // 将图像添加到我们现有的div.
-    var myIcon = new Image()
+    btn.innerHTML = 'Click me and check the console!'
+    btn.onclick = printMe
 
-    myIcon.src = Icon
+    element.appendChild(btn)
 
-    element.appendChild(myIcon)
-
-    console.log(Data)
-    
     return element
 }
 

5682f33ccef563cba432e966654d592bc48cd51c
wenliang.yang <wenliang.yang@bitdeer.com>
2021-03-23 17:07:41 +0800

    升降序，序号，列表

--- test/nextjs/nextjs-blog/pages/tictactoe.js
+++ test/nextjs/nextjs-blog/pages/tictactoe.js
@@ -13,6 +13,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)} />
     )
@@ -21,21 +22,19 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {
+          [0,1,2].map((v,k)=>{
+            return (
+              <div key={v} className="board-row">
+                {
+                  [0,1,2].map((v1, k1)=>{
+                    return this.renderSquare(v*3 + v1)
+                  })
+                }
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
@@ -46,10 +45,11 @@ class Game extends React.Component {
     super()
     this.state = {
       history: [
-        {squares: Array(9).fill(null)},
+        {squares: Array(9).fill(null),x:0,y:0},
       ],
       stepNumber: 0,
-      xIsNext: true
+      xIsNext: true,
+      shengxu: true
     }
   }
 
@@ -70,31 +70,45 @@ class Game extends React.Component {
     }
     
     squares[i] = this.state.xIsNext ? 'X' : 'O'
+    let x = Math.floor(i/3)
+    let y = i % 3
     this.setState({
       history: history.concat([{
-        squares: squares
+        squares: squares,
+        x,
+        y,
       }]),
       xIsNext: !this.state.xIsNext,
       stepNumber: history.length
     })
   }
-
+  changeOrder() {
+    this.setState({
+      shengxu: !this.state.shengxu
+    })
+  }
   
   render() {
     const history = this.state.history
     const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
-
-    const moves = history.map((step, move) => {
+   
+    let moves = history.map((step, move) => {
       const desc = move ? 'Go to move #' + move :
       'Go to game start';
-
+      console.log(move, step.x, step.y)
       return (
         <li key={move}>
-          <button onClick={()=> this.jumpTo(move)}>{desc}</button>
+          <span>{step.x}{step.y}</span>
+          <button onClick={()=> this.jumpTo(move)} 
+          className={this.state.stepNumber == move? 'hightlight btns' : 'btns'}>{desc}</button>
         </li>
       )
     })
+
+    if(!this.state.shengxu){
+      moves = moves.reverse()
+    }
 
     let status
 
@@ -116,6 +130,7 @@ class Game extends React.Component {
           <div>{status}</div>
           <div>{moves}</div>
         </div>
+        <button onClick={() => this.changeOrder()}>{this.state.shengxu? '升序' : '降序'}</button>
       </div>
     )
   }
